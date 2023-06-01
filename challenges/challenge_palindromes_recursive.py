73918da45eace105b6bddcc4fc8caa1d1dc64b63
Matheus Biaggio <matheusbiaggio@gmail.com>
2023-06-01 16:12:35 -0300

    Implementação de um palindrome com recursividade

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    if len(word) == 1:
+        return True
+    elif len(word) == 2:
+        return word[0] == word[1]
+    elif word[0] != word[-1]:
+        return False
+    else:
+        return is_palindrome_recursive(word[1:-1], low_index, high_index)
