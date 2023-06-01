f1e98352e3ee2a8b37ae0189c5b864459b34ccac
Matheus Biaggio <matheusbiaggio@gmail.com>
2023-06-01 15:42:48 -0300

    Correção complexidade do study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,16 +3,10 @@ def study_schedule(permanence_period, target_time):
         return None
 
     count = 0
-    for tupla in permanence_period:
-        first_pos, second_pos = tupla
-
-        if None in (first_pos, second_pos):
+    for first_pos, second_pos in permanence_period:
+        if not isinstance(first_pos, int) or not isinstance(second_pos, int):
             return None
-
-        if isinstance(first_pos, int) and isinstance(second_pos, int):
-            if first_pos <= target_time <= second_pos:
-                count += 1
-        else:
-            return None
+        if first_pos <= target_time <= second_pos:
+            count += 1
 
     return count
