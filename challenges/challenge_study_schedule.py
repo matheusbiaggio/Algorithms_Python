13334c2f71ed3b2e63f4a700ed54db4d6206c8b4
Matheus Biaggio <matheusbiaggio@gmail.com>
2023-06-01 15:00:56 -0300

    Implementação da função challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not permanence_period or target_time is None:
+        return None
+
+    count = 0
+    for tupla in permanence_period:
+        first_pos, second_pos = tupla
+
+        if None in (first_pos, second_pos):
+            return None
+
+        if isinstance(first_pos, int) and isinstance(second_pos, int):
+            if first_pos <= target_time <= second_pos:
+                count += 1
+        else:
+            return None
+
+    return count
